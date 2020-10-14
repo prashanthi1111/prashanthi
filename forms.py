6b6030f2d0d83cf82723559c59b1e42215859af1
prashanthi <71380685+prashanthi1111@users.noreply.github.com>
2020-10-14 18:24:33 +0530

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,13 +1,40 @@
 from django import forms
 from .models import products
 class ProductForm(forms.ModelForm):
+	title=forms.CharField(label=' ',widget=forms.TextInput(attrs={"placeholder":"your text"}))
+	email=forms.EmailField()
+	description=forms.CharField(
+		required=False,
+		widget=forms.Textarea(
+			attrs={
+			"placeholder":"your description",
+			"class":"new-class-name-two",
+			"id":"my-id-for-text-area",
+			"rows":20,
+			"cols":120
+			}
+		)
+	)
 	class Meta:
-	    model=products
-	    fields = [
-	       'title',
-	       'description',
-	       'price'
-		]
+		model=products
+		fields=[
+		'title',
+		'description',
+		'price'
+ 		]
+def clean_title(self,*args,**Kwargs):
+    title=self.cleaned_data.get("title")
+    if not "CFE" in title:
+        raise forms.ValidationError("this is not a valid title")
+    if not "ME" in title:
+        raise forms.ValidationError("this is not a valid title")
+    return title
+
+def clean_email(self,*args,**Kwargs):
+    email=self.cleaned_data.get("email")
+    if not email.endswith("edu"):      
+        raise forms.ValidationError("this is not a valid title")
+    return email	
 class RawProductForm(forms.Form):
     title=forms.CharField(label=' ',widget=forms.TextInput(attrs={"placeholder":"your text"}))
     description=forms.CharField(
--- views.py
+++ views.py
@@ -1,26 +1,61 @@
 
 from django.shortcuts import render
+from .forms import ProductForm,RawProductForm
 from .models import products
-from .forms import ProductForm
-def products_create_view(request):
-	context={}
-	return render(request,"create.html",context)
+#def products_create_view(request):
+#	my_form=RawProductForm()
+#	if request.method=="POST":
+#		my_form=RawProductForm(request.POST)
+#		if my_form.is_valid():
+#			print(my_form.cleaned_data)
+#			products.objects.create(title=my_new_title)
+#		else:
+#			print(my_form.errors)
+#	context={		'form': my_form
+#	}
+#	return render(request,"create.html",context)		
+
+
+
+
+
 
 #def products_create_view(request):
-	#form=ProductForm(request.POST or None)
-	#if form.is_valid():
-	    #form.save()
-	    #form=ProductForm()
-	#context={
-	#'form': form
-	#}
-	#return render(request,"create.html",context)
-def products_detail_view(request):
-	obj=products.objects.get(id=1)
-	context={
-	#'objects': obj
+#    if request.method=="POST":
+#    	my_new_title=request.POST.get('title')
+#    	print(my_new_title)
+#    context={}
+#    return render(request,"create.html",context)
+def render_initial_data(request):
+	initial_data={
+	    'title': "this is my new title"
 	}
-	return render(request,"detail.html",context)
+	obj=products.objects.get(id=1)
+	form=ProductForm(request.POST or None,instance=obj)
+	if form.is_valid():
+		form.save()
+	context={
+	'form': form
+	}
+	return render(request,"create.html",context)
+
+
+def products_create_view(request):
+	form=ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form=ProductForm()
+
+	context={
+	'form': form
+	}
+	return render(request,"create.html",context)
+def products_detail_view(request):
+   obj=products.objects.get(id=1)
+   context={
+   'objects':obj
+   }			
+   return render(request,"detail.html",context)
 
 
   
